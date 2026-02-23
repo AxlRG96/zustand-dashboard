0ac781c089b298a4c06fa4bdbc0a556f743cdadf
Osmar Ramos <70674854+AxlRG96@users.noreply.github.com>
2026-02-23 13:54:36 -0600

    FIX | Manejo de estados anidados importación de actions

--- src/components/organism/BearCards/BearsDisplayCard.tsx
+++ src/components/organism/BearCards/BearsDisplayCard.tsx
@@ -5,14 +5,16 @@ import { WhiteCard } from "../../shared/cards/WhiteCard";
 export const BearsDisplayCard = () => {
   const bears = useBearStore(useShallow((state) => state.bears));
   const doNothing = useBearStore((state) => state.doNothing);
+  const addBear = useBearStore((state) => state.addBear);
+  const clearBears = useBearStore((state) => state.clearBears);
   return (
     <WhiteCard centered className="px-1">
       <h1>Osos</h1>
       <button onClick={doNothing}>Do Nothing</button>
-      <button onClick={doNothing} className="mt-2">
+      <button onClick={addBear} className="mt-2">
         Agrega Oso
       </button>
-      <button onClick={doNothing} className="mt-2">
+      <button onClick={clearBears} className="mt-2">
         Limpia Oso
       </button>
       <pre className="w-full text-wrap text-xs">
